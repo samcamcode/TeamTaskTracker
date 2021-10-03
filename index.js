a6cd70a5105b3aa01f66fdda244c5034f589647c
sam <sam@pop-os.localdomain>
2021-10-03 01:04:14 -0700

    created login and register views, modfied styles, basic logic added for login and register routes

--- index.js
+++ index.js
@@ -18,10 +18,11 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.engine('ejs', ejsMate);
-app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname,'public')));
-
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 
 app.get('/', (req, res) => {
@@ -36,15 +37,40 @@ app.get('/register', (req, res) => {
     res.render('register')
 })
 
-app.get('/user', (req, res) => {
-    res.render('userHome')
+app.get('/user', async (req, res) => {
+    const tasks = await Task.find({assignedTo: '6156a55ece2a79fd364a33fd'})
+    res.render('userHome', {tasks})
 })
 
 app.get('/create', async (req, res) => {
     const users = await User.find({})
-    // console.log(users)
     res.render('createTask', {users})
 })
+
+// POSTS
+
+app.post('/login', async (req, res) => {
+    const findUser = await User.find({name: req.body.username})
+    if (findUser.length === 0) {
+        res.send('user not in db')
+    }else if(req.body.username === findUser[0].name && req.body.password === findUser[0].password) {
+        res.send('logged on')
+    }
+})
+
+app.post('/register', async (req, res) => {
+    const {username, password, email} = req.body;
+    const newUser = await new User({name: username, email, password});
+    newUser.save();
+    res.send(newUser)
+})
+
+app.post('/create', async (req, res) => {
+    res.send(req.body)
+})
+
+
+
 
 
 app.listen(port, () => {
